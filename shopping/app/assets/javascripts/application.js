6870c7c53979a6a85e3dce316554972a96bcfdc0
VenseChang <sq851023@gmail.com>
2018-01-06 14:53:40 +0800

    update order manage

--- shopping/app/assets/javascripts/application.js
+++ shopping/app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require rails-ujs
 //= require jquery
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require bootstrap/dropdown
 //= require bootstrap/alert
 //= require_tree .
--- shopping/app/controllers/orders_controller.rb
+++ shopping/app/controllers/orders_controller.rb
@@ -1,19 +1,27 @@
 class OrdersController < ApplicationController
   before_action :find_order, except: [:index]
+  # To Do
+  # 1. 評分 （含買家與賣家）
+  # 2. 更改買家評分
+  # 3. 評分的list
 
   def index
     @orders = Order.all
   end
   
   def update
-    if params.require(:order).permit(:address)['address'] == ""
-      redirect_to order_path(@order), alert: "地址未填寫"
-    else
-      if @order.update(order_param)
-        redirect_to orders_path
+    if params.key? :order
+      if params.require(:order).permit(:address)['address'] == ""
+        redirect_to order_path(@order), alert: "地址未填寫"
       else
-        render :show
+        if @order.update(order_param)
+          redirect_to orders_path
+        else
+          render :show
+        end
       end
+    else
+      @order.update(is_send: true)
     end
   end
   
